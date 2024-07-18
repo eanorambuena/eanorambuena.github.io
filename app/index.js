590941631a32e3dac8bb3c587a60a0b4c06949f3
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-07-18 01:19:17 -0400

    chore: Update Emmy.js components and landing page structure

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ export function app({ el }) {
   el.className = 'flex flex-col justify-center items-center text-center w-full h-full text-white gap-6 px-6 py-10'
 
   return html`
-    <header class='flex flex-col justify-center items-center selection:text-center w-full h-full text-white gap-6 p-6'>
-      <h1 class='text-8xl font-bold'>Emmanuel Norambuena</h1>
-      <p class='text-2xl'>Fullstack Developer</p>
+    <header class='flex flex-row justify-center items-center selection:text-center w-full h-full text-white gap-6 p-6'>
+      <div class='flex flex-col justify-left items-center text-center gap-2'>
+        <h1 class='text-6xl font-bold'>Emmanuel Norambuena</h1>
+        <p class='text-4xl'>Fullstack Developer</p>
+      </div>
+      <img class='w-40 h-40 rounded-full' src='https://emmyjs.pages.dev/eanorambuena.webp' alt='Emmanuel Norambuena' />
     </header>
-
     <main class='flex flex-col justify-center items-center text-center w-full h-full text-white gap-6 p-6'>
       <h2 class='text-3xl font-bold text-purple-500'>Projects</h2>
       <section class='grid grid-cols-1 gap-4 p-4 md:grid-cols-2'>
